53a2febfee519921ae748a175265684cb1676d51
Rodrigo Fraga <rodrigo.fraga@gmail.com>
2013-12-22 21:12:14 -0500

    Adds song player metadata class

---
+++ SongRequest.SongPlayer/ISongPlayerData.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SongRequest.SongPlayer
+{
+    public interface ISongPlayerData
+    {
+        PlatformID Platform { get; }
+        string PlayerType { get; }
+    }
+}
